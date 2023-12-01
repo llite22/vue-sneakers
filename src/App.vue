7d7c10c920048946dcce7dcd937e0dbdc6047040
llite22 <llite2@yandex.ru>
2023-12-01 22:11:04 +0300

    header

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+    <Header />
+  </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+  <template>
+      <header class="flex justify-between border-b border-slate-300 px-10 py-8">
+        <div class="flex items-center">
+          <img width="40" src="/logo.png" alt="Logo" />
+          <div>
+            <h2 class="text-xl font-bold uppercase">Vue Sneakers</h2>
+            <p class="text-slate-500">The best sneaker store</p>
+          </div>
+        </div>
+        <ul class="flex items-center gap-10">
+          <li class="flex items-center gap-3 cursor-pointer text-gray-500 hover:text-black">
+            <img src="/cart.svg" alt="Cart" />
+            <b>1205 руб.</b>
+          </li>
+          <li class="flex items-center gap-3 cursor-pointer text-gray-500 hover:text-black">
+            <img src="/heart.svg" alt="Heart" />
+            <span>Закладки</span>
+          </li>
+          <li class="flex items-center gap-3 cursor-pointer text-gray-500 hover:text-black">
+            <img src="/profile.svg" alt="Profile" />
+            <span>Профиль</span>
+          </li>
+        </ul>
+      </header>
+  </template>
+
